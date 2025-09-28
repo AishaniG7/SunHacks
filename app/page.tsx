d9ee0bd7f5bb83518d58dbe1d6115ec049dda471
ArbitraryAlpaca <aaditcha007@gmail.com>
2025-09-28 04:34:29 -0700

    new ico file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-"use client"; // Required for useState and other React hooks
-
+"use client";
 import React, { useState } from "react";
-
-import { useState } from "react";
-import  ReactMarkdown from "react-markdown";
+import ReactMarkdown from "react-markdown";
 
 export default function Home() {
   const [topic, setTopic] = useState("");
@@ -25,11 +22,15 @@ export default function Home() {
     try{
       const res = await fetch("/api/generateTopic");
       const data = await res.json();
-    console.log(typeof(data.topic));
-      setTopic(data.topic);
+
+      let cleanTopic = data.topic.includes('"')
+        ? data.topic.split('"').slice(1).join('"').replace(/"/g, "")
+        : data.topic.replace(/"/g,"");
+
+      setTopic(cleanTopic.replace(/\*/g,""));
       setEssay("");
       setFeedback("");
-    //setCustomTopic("");
+      setCustomTopic("");
     } catch(error) {
       console.error("Error generating topic:", error);
     } finally {
@@ -38,9 +39,9 @@ export default function Home() {
   };
 
   const gradeEssay = async () => {
-
+    
     const chosenTopic = customTopic !== "" ? customTopic : topic;
-    console.log("CHOSEN TOPIC: ", chosenTopic )
+    console.log("CHOSEN TOPIC: ", chosenTopic );
 
     setIsGrading(true);
     try{
@@ -60,25 +61,25 @@ export default function Home() {
   };
 
   return (
-    <div style ={{fontFamily: 'Helvetica', fontSize:"100%"}} className="h-15 mx-10 rounded-xl border-3 my-4 p-2 text-center bg-[#0f494fac]">
+    <div style ={{fontFamily: 'Helvetica', fontSize:"110%"}} className="h-15 mx-10 rounded-xl border-3 my-4 p-2 text-center bg-[#0f494fac]">
       <h5 className="font-bold text-4xl text-center">AI WRITING TUTOR</h5>
-      <button onClick={generateTopic} className={"my-10 mx-2 text-2xl transition duration-300 ease-in-out hover:bg-[#748d92] bg-[#003135] text-[#AFDDE5] px-5 py-2 rounded-full cursor-pointer font-['Helvetica']"} disabled={isGenerating}> 
+      
+      <textarea
+        style={{ fontFamily: 'Helvetica' }}
+        rows = {5}
+        cols={50}
+        placeholder="Enter your custom prompt here:"
+        className="mx-auto my-15 flex border-2 border-[#d3d9d4] p-2 rounded-lg text-[100%]"
+        value={customTopic}
+        onChange={(e) => setCustomTopic(e.target.value)}
+      />
+      <button onClick={generateTopic} className={"mt-5 mb-5 text-2xl transition duration-300 ease-in-out hover:bg-[#748d92] bg-[#003135] text-[#AFDDE5] px-5 py-2 rounded-full cursor-pointer font-['Helvetica']"} disabled={isGenerating}> 
         {isGenerating ? (
         <div className="animate-spin [animation-duration: s] rounded-full h-5 w-5 border-b-2 border-white"></div>
       ) : ('Generate Topic')}
       </button>
-    <div style={{ padding: "2rem", maxWidth: "800px", margin: "auto" }}>
-      <h1>AI Writing Tutor</h1>
-      <textarea
-        rows = {3}
-        cols={80}
-        placeholder="Enter your custom prompt here:"
-        value = {customTopic}
-        onChange={(e) => setCustomTopic(e.target.value)}
-      />
-      <button onClick={generateTopic}>Generate Topic</button>
 
-      {topic && <h2 className="my-5"style={{ fontFamily: 'Helvetica', fontSize: '150%' }}>Topic: {topic}</h2>}
+      {topic && customTopic === "" && (<h2 className="my-5" style={{ fontFamily: 'Helvetica', fontSize: '150%' }}><span className={"font-bold "}>Topic: </span>{topic}</h2>)}
 
       {topic && (
         <>
@@ -86,7 +87,7 @@ export default function Home() {
             style={{ fontFamily: 'Helvetica' }}
             rows={10}
             cols={50}
-            className="mx-auto my-5 border-2 border-[#d3d9d4] p-2 rounded-lg text-[120%]"
+            className="mx-auto flex my-5 border-2 border-[#d3d9d4] p-2 rounded-lg text-[120%]"
             placeholder="Write your essay here..."
             value={essay}
             onChange={(e) => setEssay(e.target.value)}
@@ -94,7 +95,7 @@ export default function Home() {
           <br />
           <button onClick={gradeEssay} className={"my-5 mx-2 text-2xl transition duration-300 ease-in-out hover:bg-[#748d92] bg-[#003135] text-[#AFDDE5] px-5 py-2 rounded-full cursor-pointer font-['Helvetica']"} disabled={isGrading}>
            {isGrading ? (
-    <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white"></div>
+    <div className="animate-spin flex rounded-full h-5 w-5 border-b-2 border-white"></div>
   ) : (
     'Submit Essay'
   )}</button>
@@ -118,5 +119,5 @@ export default function Home() {
         </div>
       )}
     </div>
-  );
-}
+  
+    );}
